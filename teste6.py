d7e81986e7c47ba30cc3a8664dc91da9e59199f3
Programador250 <67872561+Programador250@users.noreply.github.com>
2020-07-05 15:12:08 -0300

    Add files via upload

---
+++ teste6.py
@@ -0,0 +1,7 @@
+print = input('Oie, Boa Tarde!')
+print = input('Tudo bem? ')
+print = input('Que bom, Eu também estou bem, obrigado!')
+print = input('Qual é o seu nome? ')
+print = input('Que bom, o meu nome é Rex')
+print = input('Quantos anos você tem? ')
+print = input('Eu tenho 25 anos')
